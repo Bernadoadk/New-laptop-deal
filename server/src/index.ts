5ea633a3093f0499a57e06923a1b35d1d0e5c1d3
Bernadoadk <bernado.adikpeto@epitech.eu>
2026-04-11 16:42:26 +0100

    first commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -20,7 +20,10 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet({
   crossOriginResourcePolicy: false,
 }));
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 
